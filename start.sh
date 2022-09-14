040da132566e57a0f65afffa2080a93c6c04f931
aidinattar <attaraidin@gmail.com>
2022-09-14 21:58:58 +0000

    correct producer and spark computations

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 
 rm -r tmp/kafka-logs
 
-for run in {1..10}; do
-  lsof -ti:2181 | xargs kill -9
-done
+#for run in {1..10}; do
+#  lsof -ti:2181 | xargs kill -9
+#done
 
 # Start Spark Cluster
-sh /usr/local/spark/sbin/start-all.sh
+/usr/local/spark/sbin/start-all.sh
 
 sleep 1
 
 # Start Zookeeper
-sh /usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties &
+/usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties &
 
 sleep 1
 
 # Start Kafka
-sh /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties &
+/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties &
--- stop.sh
+++ stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Stop Spark Cluster
-sh /usr/local/spark/sbin/stop-all.sh
+/usr/local/spark/sbin/stop-all.sh
 
 sleep 1
 
 # Stop Zookeeper
-sh /usr/local/kafka/bin/zookeeper-server-stop.sh
+/usr/local/kafka/bin/zookeeper-server-stop.sh
 
-sleep 10
+sleep 1 
 
 # Stop Kafka
-sh /usr/local/kafka/bin/kafka-server-stop.sh
+/usr/local/kafka/bin/kafka-server-stop.sh
